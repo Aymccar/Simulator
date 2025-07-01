0785eaa4c6a3a658ae96e927d2a6dd21f07bb191
Aymeric <aymericardot@gmail.com>
2025-07-01 18:18:14 +0200

    Adding general launch file TODO pass everything by args

--- README.md
+++ README.md
@@ -64,16 +64,22 @@ Run ```Tools/environment_install/install-prereqs-ubuntu.sh -y``` on the root
 ```bash
 cd ardupilot 
 ./waf configure --board ros --toolchain native --wafrosbridge_include_path  
-"{PATH to this repo}/colcon_ws/src/wafros2bridge/include" --wafrosbridge_lib_path "{PATH to this repo}/colcon_ws/install/wafros2bridge/lib"
+"{PATH to this repo}/colcon_ws/src/Waf_Ros2_Bridge/include" --wafrosbridge_lib_path "{PATH to this repo}/colcon_ws/install/Waf_Ros2_Bridge/lib"
 ./waf sub
 ```
 
 ## Running the simulator
-First launch BlueOS:
+Create to v4l2 loopback camera:
+```bash
+modprobe v4l2loopback   devices=2   video_nr=42,43   card_label="VirtualCam42,VirtualCam43"   exclusive_caps=1,1
+```
+
+Launch BlueOS:
 ```bash
 cd BlueOS
 docker compose -f core/compose/compose.yml up
 ``` 
+
 Then access to **localhost** in a browser.
 Click on the robot face on left top and enable **Pirate Mode**.
 Go to **Autopilot Firmware**.
@@ -82,15 +88,9 @@ Click on **UPLOAD FIRMWARE FILE** and upload the freshly compiled firmware locat
 
 Then run: 
 ```bash 
-ros2 launch ArdupilotStonefish parser_launch.yaml
+ros2 launch simulator simulator.launch.py
 ```
-and finally run Stonefish with:
-```bash
-ros2 launch stonefish_ros2 launchros.launch.py #Add stonefish args if needed
-```
-For the video launch :
-```bash
-ros2 run v4l2ros2 reset.sh #Need sudo password
-ros2 launch v4l2ros2 video_launch.py
-```
+Now you should be able to add a stream on BlueOS like on a normal robot. You should also be able to control the robot from any kind of GCScompatible with MavLink and ardupilot like [QGroundControl](https://qgroundcontrol.com/) or [BlueRobotics Cockpit](https://github.com/bluerobotics/cockpit).  
 
+[!NOTE]
+Path are hard coded for the moment, soon it will become args.
---
+++ colcon_ws/src/simulator/launch/simulator.launch.py
@@ -0,0 +1,64 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    path_simulator = "/home/aymeric/simulator" #TODO args
+
+    # get stonefish launch file
+    stonefish_path = os.path.join(
+        FindPackageShare("stonefish_ros2").find("stonefish_ros2"),
+        "launch",
+        "stonefish_simulator.launch.py"
+    )
+
+    # get Ardupilot ROS2 bridge launch file
+    ardupilotros2_path = os.path.join(
+        FindPackageShare("ArdupilotStonefish").find("ArdupilotStonefish"),
+        "launch",
+        "parser_launch.yaml"
+    )
+
+    # create v4l2 loopback commands HAS TO BE DONE NOT HERE
+    #v4l2_cmd = """
+    #    echo "Creating v4l2 loopback camera:";
+    #    sudo modprobe -r v4l2loopback;
+    #    sudo modprobe v4l2loopback devices=2 video_nr=42,43 card_label="VirtualCam42,VirtualCam43" exclusive_caps=1,1;
+    #"""
+
+    # get v4l2 ROS2 package launch file
+    v4l2_path = os.path.join(
+        FindPackageShare("v4l2ros2").find("v4l2ros2"),
+        "launch",
+        "video_launch.py"
+    )
+
+    # raytracer bash command
+    raytracer_cmd = f"""
+        echo "Starting raytracer:";
+        {path_simulator}/colcon_ws/install/raytracer/lib/raytracer/raytracer;
+    """
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(stonefish_path),
+            launch_arguments={"simulation_data": f"{path_simulator}/data/other/data",
+                              "scenario_desc": f"{path_simulator}/data/other/scenarios/bluerov_heavy_simplified_shipwreck.scn",
+                              "window_res_x": "800",
+                              "window_res_y": "600",
+                              "simulation_rate": "1000"}.items()
+        ),  
+        IncludeLaunchDescription(
+            FrontendLaunchDescriptionSource(ardupilotros2_path),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(v4l2_path),
+        ),
+        ExecuteProcess(
+            cmd=['bash', '-c', raytracer_cmd],
+            shell=True,
+            output='screen'
+        )
+    ])
