90af5262960a5dce94572f59743f7f763956a517
Aymeric <aymericardot@gmail.com>
2025-07-02 14:09:36 +0200

    everything by args and renaming of the bridge

--- colcon_ws/src/simulator/launch/simulator.launch.py
+++ colcon_ws/src/simulator/launch/simulator.launch.py
@@ -1,51 +1,56 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.substitutions import FindPackageShare, FindPackagePrefix
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 import os
 
 def generate_launch_description():
-    path_simulator = "/home/aymeric/simulator" #TODO args
+    # ROS2 args
+    scenario_path_arg = DeclareLaunchArgument("scenario_desc", default_value="", description="...")
+    simulation_data_arg = DeclareLaunchArgument("simulation_data", default_value="", description="...")
+    
+    scenario_path = LaunchConfiguration("scenario_desc")
+    data_path = LaunchConfiguration("simulation_data")
+            
 
     # get stonefish launch file
-    stonefish_path = os.path.join(
-        FindPackageShare("stonefish_ros2").find("stonefish_ros2"),
+    stonefish_path = PathJoinSubstitution([
+        FindPackageShare("stonefish_ros2"),
         "launch",
         "stonefish_simulator.launch.py"
+        ]
     )
 
     # get Ardupilot ROS2 bridge launch file
-    ardupilotros2_path = os.path.join(
-        FindPackageShare("ardupilotstonefish").find("ardupilotstonefish"),
+    ardupilotros2_path = PathJoinSubstitution([
+        FindPackageShare("ardupilotstonefish"),
         "launch",
         "parser_launch.yaml"
+        ]
     )
 
-    # create v4l2 loopback commands HAS TO BE DONE NOT HERE
-    #v4l2_cmd = """
-    #    echo "Creating v4l2 loopback camera:";
-    #    sudo modprobe -r v4l2loopback;
-    #    sudo modprobe v4l2loopback devices=2 video_nr=42,43 card_label="VirtualCam42,VirtualCam43" exclusive_caps=1,1;
-    #"""
-
     # get v4l2 ROS2 package launch file
-    v4l2_path = os.path.join(
-        FindPackageShare("v4l2ros2").find("v4l2ros2"),
+    v4l2_path = PathJoinSubstitution([
+        FindPackageShare("v4l2ros2"),
         "launch",
         "video_launch.py"
+        ]
     )
 
     # raytracer bash command
-    raytracer_cmd = f"""
-        echo "Starting raytracer:";
-        {path_simulator}/colcon_ws/install/raytracer/lib/raytracer/raytracer;
-    """
+    path_raytracer = PathJoinSubstitution([FindPackagePrefix("raytracer"), "lib", "raytracer", "raytracer"])
+    raytracer_cmd = [path_raytracer,
+                     scenario_path,
+                     data_path]
 
     return LaunchDescription([
+        scenario_path_arg,
+        simulation_data_arg,
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(stonefish_path),
-            launch_arguments={"simulation_data": f"{path_simulator}/data/other/data",
-                              "scenario_desc": f"{path_simulator}/data/other/scenarios/bluerov_heavy_simplified_shipwreck.scn",
+            launch_arguments={"simulation_data": data_path,
+                              "scenario_desc": scenario_path, 
                               "window_res_x": "800",
                               "window_res_y": "600",
                               "simulation_rate": "1000"}.items()
@@ -57,8 +62,7 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(v4l2_path),
         ),
         ExecuteProcess(
-            cmd=['bash', '-c', raytracer_cmd],
-            shell=True,
+            cmd = raytracer_cmd,
             output='screen'
         )
     ])
